2372a66b240cc667022b2af629d228570e73a1c4
Wwright91 <wisdomwright-martin@pursuit.org>
2024-04-02 21:05:31 -0400

    Fetched data from the API 1. Made an async fetch to the backend url using useeffect 2. Created state for the raffle data 3. Added .env to .gitignore file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [raffles, setRaffles] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`${API_URL}/raffles`);
+      const { data } = await res.json();
+      if (res.ok) {
+        setRaffles(data);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
+      {raffles.map(({ name, id }) => {
+        return <p key={id}>{name}</p>;
+      })}
     </div>
   );
 }
