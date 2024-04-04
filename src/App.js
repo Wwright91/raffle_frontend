94e57606165b4c8c44c6d9cd4339fdacb9fb1c29
Wwright91 <wisdomwright-martin@pursuit.org>
2024-04-04 12:07:28 -0400

    Created form for adding a new raffle 1. Created state for form 2. Added form and inputs 3. Implemented onChange and create raffle functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [raffles, setRaffles] = useState([]);
+  const [form, setForm] = useState({ name: "", secret_token: "" });
 
   useEffect(() => {
     async function fetchData() {
@@ -17,8 +18,64 @@ function App() {
     fetchData();
   }, []);
 
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.id]: e.target.value });
+  };
+
+  async function createNewRaffle() {
+    try {
+      const createRaffle = await fetch(`${API_URL}/raffles`, {
+        method: "POST",
+        body: JSON.stringify(form),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (createRaffle.ok) {
+        const data = await createRaffle.json();
+        console.log("created succesfully", data);
+        setForm({ name: "", secret_token: "" });
+      } else {
+        throw new Error("Raffle not created");
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // ensure form is being cleared
+  useEffect(() => {
+    console.log("Form state after render:", form);
+  }, [form]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewRaffle();
+  };
+
   return (
     <div className="App">
+      <h2>New Raffle</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Raffle Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={form.name}
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="secret_token">Raffle Secret Token:</label>
+        <input
+          type="text"
+          id="secret_token"
+          value={form.secret_token}
+          required
+          onChange={handleChange}
+        />
+        <button>Create New Raffle</button>
+      </form>
       {raffles.map(({ name, id }) => {
         return <p key={id}>{name}</p>;
       })}
