56a40a7a2b0dbd9e53d12aba3fe4a13177abc775
Wwright91 <wisdomwright-martin@pursuit.org>
2024-04-07 01:19:44 -0400

    New participant 1. Created component for new participants 2. Imported component into RaffleDetails.js
    There are no errors in the console right now.

---
+++ src/components/newParticipant/NewParticipant.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+const NewParticipant = ({ id }) => {
+  const [form, setForm] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone: "",
+  });
+
+  // console.log("raffle_id", raffle_id)
+
+  async function enterNewParticipant() {
+    try {
+      const createParticipant = await fetch(
+        `${API_URL}/raffles/${id}/participants`,
+        {
+          method: "POST",
+          body: JSON.stringify(form),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      // console.log("new creation", createParticipant)
+
+      if (createParticipant.ok) {
+        clearForm();
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  const clearForm = () => {
+    setForm({ first_name: "", last_name: "", email: "", phone: "" });
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    enterNewParticipant();
+  };
+
+  return (
+    <div>
+      <h4>Register to participate in the raffle:</h4>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first_name">First Name:</label>
+        <input
+          type="text"
+          id="first_name"
+          value={form.first_name}
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="last_name">Last Name:</label>
+        <input
+          type="text"
+          id="last_name"
+          value={form.last_name}
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={form.email}
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="phone">Phone:</label>
+        <input
+          type="text"
+          id="phone"
+          value={form.phone}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+        <button onClick={clearForm}>Reset</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewParticipant;
--- src/components/raffleDetails/RaffleDetails.js
+++ src/components/raffleDetails/RaffleDetails.js
@@ -1,13 +1,16 @@
 import { Button } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import NewParticipant from "../newParticipant/NewParticipant";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 const RaffleDetails = () => {
   const [raffleDetails, setRaffleDetails] = useState([]);
+  const [view, setView] = useState("register");
 
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchData() {
@@ -26,11 +29,19 @@ const RaffleDetails = () => {
     <div>
       <h4>{raffleDetails.name}</h4>
       <nav>
-        <Button>All Raffles</Button>
-        <Button>Register</Button>
-        <Button>Participants</Button>
-        <Button>Pick Winner</Button>
+        <Button onClick={() => navigate("/")}>All Raffles</Button>
+        <Button onClick={() => setView("register")}>Register</Button>
+        <Button onClick={() => setView("participants")}>Participants</Button>
+        <Button onClick={() => setView("winner")}>Pick Winner</Button>
       </nav>
+      {view === "register" && (
+        <div>
+          Register Participant
+          <NewParticipant id={id} />
+        </div>
+      )}
+      {view === "participants" && <div>Participants</div>}
+      {view === "winner" && <div>Winner</div>}
     </div>
   );
 };
