4979da4a5312b373101a8ed600c5c030c05d4213
Wwright91 <wisdomwright-martin@pursuit.org>
2024-04-14 03:42:05 -0400

    Styled form for registering a new participant

--- src/components/newParticipant/NewParticipant.js
+++ src/components/newParticipant/NewParticipant.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import "./NewParticipant.scss";
+
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import FormControl from "@mui/material/FormControl";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
@@ -13,8 +20,6 @@ const NewParticipant = () => {
 
   const { id } = useParams();
 
-  // console.log("raffle_id", id)
-
   async function enterNewParticipant() {
     try {
       const createParticipant = await fetch(
@@ -27,8 +32,6 @@ const NewParticipant = () => {
           },
         }
       );
-
-      // console.log("new creation", createParticipant)
 
       if (createParticipant.ok) {
         clearForm();
@@ -53,41 +56,63 @@ const NewParticipant = () => {
 
   return (
     <div>
-      <h5>Register to participate in the raffle:</h5>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="first_name">First Name:</label>
-        <input
-          type="text"
-          id="first_name"
-          value={form.first_name}
-          required
-          onChange={handleChange}
-        />
-        <label htmlFor="last_name">Last Name:</label>
-        <input
-          type="text"
-          id="last_name"
-          value={form.last_name}
-          required
-          onChange={handleChange}
-        />
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          id="email"
-          value={form.email}
-          required
-          onChange={handleChange}
-        />
-        <label htmlFor="phone">Phone:</label>
-        <input
-          type="text"
-          id="phone"
-          value={form.phone}
-          onChange={handleChange}
-        />
-        <button>Submit</button>
-        <button onClick={clearForm}>Reset</button>
+        <h4>Register to participate in the raffle:</h4>
+        <div className="Participants">
+          <FormControl sx={{ width: "50%" }}>
+            <InputLabel htmlFor="first_name">First Name:*</InputLabel>
+            <Input
+              type="text"
+              id="first_name"
+              value={form.first_name}
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+          <FormControl sx={{ width: "50%" }}>
+            <InputLabel htmlFor="last_name">Last Name:*</InputLabel>
+            <Input
+              type="text"
+              id="last_name"
+              value={form.last_name}
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+        </div>
+        <FormControl>
+          <InputLabel htmlFor="email">Email:*</InputLabel>
+          <Input
+            type="text"
+            id="email"
+            value={form.email}
+            required
+            onChange={handleChange}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="phone">Phone:</InputLabel>
+          <Input
+            type="text"
+            id="phone"
+            value={form.phone}
+            onChange={handleChange}
+          />
+        </FormControl>
+        <div className="Buttons">
+          <ButtonGroup>
+            <Button type="submit" sx={{ bgcolor: "grey", color: "white" }}>
+              Submit
+            </Button>
+            <Button
+              type="submit"
+              sx={{ bgcolor: "black", color: "white" }}
+              onClick={clearForm}
+            >
+              Reset
+            </Button>
+          </ButtonGroup>
+        </div>
       </form>
     </div>
   );
