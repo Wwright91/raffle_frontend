c2bf711e0367e38539fbf1b1952b3e8dc183cbd7
Wwright91 <wisdomwright-martin@pursuit.org>
2024-04-14 02:53:16 -0400

    Added styling to list of raffles

--- src/components/raffle/Raffle.js
+++ src/components/raffle/Raffle.js
@@ -1,18 +1,26 @@
 import React from "react";
+import "./Raffle.scss";
 import { Link } from "react-router-dom";
 
 const Raffle = ({ raffle }) => {
-  const { name, created_on, winner_id, raffled_on, id } = raffle;
+  const { name, winner_id, created_on, raffled_on, id } = raffle;
 
   return (
-    <div key={id}>
+    <div key={id} className="Raffle">
       <Link to={`/raffles/${id}`}>
         <h4>{name}</h4>
-        <p>Created on: {created_on}</p>
-        <p>Winner Id: {winner_id !== null ? winner_id : "No one yet"}</p>
         <p>
+          <span className="material-symbols-outlined">calendar_add_on</span>{" "}
+          Created on: {new Date(created_on).toString()}
+        </p>
+        <p>
+          <span className="material-symbols-outlined">trophy</span> Winner Id:{" "}
+          {winner_id !== null ? winner_id : "No one yet"}
+        </p>
+        <p>
+          <span className="material-symbols-outlined">event_available</span>
           {raffled_on !== null
-            ? `Raffled on: ${raffled_on}`
+            ? `Raffled on: ${new Date(raffled_on)}`
             : "Not raffled yet"}
         </p>
       </Link>
--- src/components/raffles/Raffles.js
+++ src/components/raffles/Raffles.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Raffles.scss";
 import Raffle from "../raffle/Raffle";
 
 const Raffles = ({ raffles }) => {
   return (
-    <div>
-      <h3>Raffles</h3>
+    <div className="Raffles">
+      <h3>All Raffles:</h3>
       {raffles.map((raffle, id) => {
         return <Raffle raffle={raffle} key={id} />;
       })}
